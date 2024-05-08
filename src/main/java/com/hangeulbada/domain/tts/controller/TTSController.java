f5b58cf58cdb3639ec1279672733b8cb0b64d0e5
llshyun27 <girl882@naver.com>
2024-05-08 15:00:31 +0900

    feat/tts

--- src/main/java/com/hangeulbada/domain/tts/controller/TTSController.java
+++ src/main/java/com/hangeulbada/domain/tts/controller/TTSController.java
@@ -1,22 +1,35 @@
 package com.hangeulbada.domain.tts.controller;
 
 import com.hangeulbada.domain.tts.service.TTSService;
+import com.hangeulbada.domain.workbookset.dto.QuestionDto;
+import com.hangeulbada.domain.workbookset.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tts")
 @RequiredArgsConstructor
 public class TTSController {
     private final TTSService ttsService;
+    private final QuestionService questionService;
 
     @PostMapping("/tts")
     public ResponseEntity<?> submit2() {
         String ans = ttsService.tts();
-
         return ResponseEntity.ok(ans);
+    }
+    @GetMapping("/tts/{questionId}")
+    public ResponseEntity<QuestionDto> getQuestionById(@PathVariable(name="workbookId")String workbookId,
+                                                       @PathVariable(name="questionId")String questionId){
+        String audioFilePath = ttsService.tts();
+        if(StringUtils.isEmpty(audioFilePath)) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        QuestionDto questionDto = questionService.getQuestionById(workbookId, questionId);
+        questionDto.setAudioFilePath(audioFilePath);
+        return new ResponseEntity<>(questionDto, HttpStatus.OK);
     }
 }
