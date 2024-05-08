5e98b0d0399f5817387c706c794e42cf0655d92f
ssoxong <ssoxong@naver.com>
2024-05-08 15:23:57 +0900

    feat: 로그인 회원가입 통합

--- src/main/java/com/hangeulbada/domain/auth/controller/AuthController.java
+++ src/main/java/com/hangeulbada/domain/auth/controller/AuthController.java
@@ -5,9 +5,6 @@ import com.hangeulbada.domain.auth.dto.LoginResponse;
 import com.hangeulbada.domain.auth.dto.SignupResponse;
 import com.hangeulbada.domain.auth.service.GoogleService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,10 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -28,46 +25,52 @@ public class AuthController {
 
     private final GoogleService googleService;
 
-    @ResponseBody
-    @GetMapping("/login/oauth2/code/google")
-    @UnsecuredAPI
-    @Operation(summary = "구글 로그인", description = "구글 로그인 후 redirect되는 back url")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "로그인 성공",
-            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
-            @ApiResponse(responseCode = "401", description = "회원가입 필요",
-            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
-            @ApiResponse(responseCode = "404", description = "유효하지 않은 인가 code")
-    })
-    public ResponseEntity<?> googleRedirectUri(
-            @Parameter(description = "구글 로그인 후 리다이렉트 URI로 전달받은 code") @RequestParam String code)
-    {
-        try {
-            LoginResponse response = googleService.googleOauth2(code);
-            // 로그인 성공
-            if (response.getToken()!=null){
-                return ResponseEntity.ok(response);
-            }
-            // 회원가입 필요
-            else{
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-            }
-        }
-        // 유효하지 않은 code
-        catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유효하지 않은 code입니다.");
-        }
-    }
+//    @ResponseBody
+//    @GetMapping("/login/oauth2/code/google")
+//    @UnsecuredAPI
+//    @Operation(summary = "구글 로그인", description = "구글 로그인 후 redirect되는 back url")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "로그인 성공",
+//            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+//            @ApiResponse(responseCode = "401", description = "회원가입 필요",
+//            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+//            @ApiResponse(responseCode = "404", description = "유효하지 않은 인가 code")
+//    })
+//    public ResponseEntity<?> googleRedirectUri(
+//            @Parameter(description = "구글 로그인 후 리다이렉트 URI로 전달받은 code") @RequestParam String code)
+//    {
+//        try {
+//            LoginResponse response = googleService.googleOauth2(code);
+//            // 로그인 성공
+//            if (response.getToken()!=null){
+//                return ResponseEntity.ok(response);
+//            }
+//            // 회원가입 필요
+//            else{
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//            }
+//        }
+//        // 유효하지 않은 code
+//        catch (NoSuchElementException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "유효하지 않은 code입니다.");
+//        }
+//    }
 
     @ResponseBody
-    @Operation(summary = "구글 회원가입", description = "구글 로그인 시 response 했던 정보에서 role 선택 후 회원가입")
+    @Operation(summary = "구글 로그인/회원가입", description = "구글 로그인 시 response 했던 정보에서 role 선택 후 회원가입")
     @PostMapping("/login/oauth2/code/google/signup")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "회원가입 성공")
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "201", description = "회원가입 성공")
+
     })
     @UnsecuredAPI
     public ResponseEntity<LoginResponse> googleSignup(@RequestBody SignupResponse signupResponse) {
-        return ResponseEntity.ok(googleService.googleSignup(signupResponse));
-
+        if(googleService.isRegisteredUser(signupResponse.getUid())){
+            //로그인
+            return new ResponseEntity<>(googleService.googleSignin(signupResponse), HttpStatus.OK);
+        }
+        //회원가입
+        return new ResponseEntity<>(googleService.googleSignup(signupResponse), HttpStatus.CREATED);
     }
 }
