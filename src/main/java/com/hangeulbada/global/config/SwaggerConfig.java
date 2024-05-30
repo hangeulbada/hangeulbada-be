7d1fc4b6d4ea8ecfefc9c996552cb2f9720c7868
So Hyeon <ssoxong@naver.com>
2024-05-30 14:55:48 +0900

    feat/ssoxong (#61)
    * Update README.md
    
    * feat: application secret
    
    * Update main.yml
    
    * Update README.md
    
    * Update main.yml
    
    * feat: workflow
    
    * Update main.yml
    
    * Update README.md (#55)
    
    * feat: main
    
    * feat: add setup yml
    
    * feat: linew
    
    * feat: submitDTO 순서

--- src/main/java/com/hangeulbada/global/config/SwaggerConfig.java
+++ src/main/java/com/hangeulbada/global/config/SwaggerConfig.java
@@ -12,10 +12,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
 
-@OpenAPIDefinition(info = @Info(title = "한글바다 API 명세서"),
-        servers = {@Server(url = "https://ssoxong.xyz", description = "한글바다 HTTPS API 서버"),
-                    @Server(url = "http://ssoxong.xyz", description = "한글바다 HTTP API 서버"),
-                    @Server(url = "http://ec2-43-201-97-164.ap-northeast-2.compute.amazonaws.com:5943/", description = "한글바다 HTTP API 서버 (EC2)")})
+// 로컬서버 실행시 주석처리
+@OpenAPIDefinition(info = @Info(title = "한글바다 API 명세서"), servers = {@Server(url = "https://ssoxong.xyz", description = "한글바다 HTTPS API 서버")})
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -29,4 +27,5 @@ public class SwaggerConfig {
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .security(Collections.singletonList(securityRequirement));
     }
+
 }
