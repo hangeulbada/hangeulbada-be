eff34d659acb781135bf147abbb62f471bd9a64f
So Hyeon <ssoxong@naver.com>
2024-05-31 16:28:17 +0900

    Update SwaggerConfig.java

--- src/main/java/com/hangeulbada/global/config/SwaggerConfig.java
+++ src/main/java/com/hangeulbada/global/config/SwaggerConfig.java
@@ -1,4 +1,5 @@
 package com.hangeulbada.global.config;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
