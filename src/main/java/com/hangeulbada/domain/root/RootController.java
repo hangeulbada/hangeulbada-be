c90a67162660993a8bcc8890f6f402d3cb951363
So Hyeon <ssoxong@naver.com>
2024-11-07 00:51:17 +0900

    Update RootController.java

--- src/main/java/com/hangeulbada/domain/root/RootController.java
+++ src/main/java/com/hangeulbada/domain/root/RootController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
     @GetMapping("/")
     public String root() {
-        return "hangeul bada HTTPS API\n 한글바다 API 서버입니다.";
+        return "hangeul bada HTTPS API\n 한글바다 HTTPS API 서버입니다.";
     }
 }
