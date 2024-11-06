a777d1843456c307eca4915090d8bb3dfcbe90ad
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2024-11-06 10:39:15 +0900

    refactor: 루트 변경

--- src/main/java/com/hangeulbada/DemoApplication.java
+++ src/main/java/com/hangeulbada/DemoApplication.java
@@ -1,14 +1,8 @@
 package com.hangeulbada;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@OpenAPIDefinition(servers = {
-		@Server(url = "/", description = "Default Server URL")
-})
 
 @EnableMongoRepositories
 @SpringBootApplication
